4a1b60234aa31dc08787abc362b55d6566209802
hskwakr <33633391+hskwakr@users.noreply.github.com>
2020-12-11 11:19:19 +0900

    Add codes of BeforeISP
    and tests

---
+++ Test/Sample1/BeforeISP/TestHPLaserJetPrinter.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using PracticeInterfaceSegregationPrinciple.Sample1.BeforeISP;
+
+namespace Test.Sample1.BeforeISP
+{
+    public class TestHPLaserJetPrinter
+    {
+        public class FaxMethod
+        {
+            [Fact]
+            public void Fax_Calculated()
+            {
+                HPLaserJetPrinter printer = new HPLaserJetPrinter();
+                string content = "I am a test.";
+                string expected = "HPLaserJetPrinter Fax : " + content;
+
+                Assert.Equal(expected, printer.Fax(content));
+            }
+        }
+    }
+}
---
+++ Test/Sample1/BeforeISP/TestLiquidInkjetPrinter.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using PracticeInterfaceSegregationPrinciple.Sample1.BeforeISP;
+
+namespace Test.Sample1.BeforeISP
+{
+    public class TestLiquidInkjetPrinter
+    {
+        public class FaxMethod
+        {
+            [Fact]
+            public void Fax_ThrowException()
+            {
+                LiquidInkjetPrinter printer = new LiquidInkjetPrinter();
+                string content = "I am a test.";
+
+                Assert.Throws<System.NotImplementedException>(() => printer.Fax(content));
+            }
+        }
+    }
+}
